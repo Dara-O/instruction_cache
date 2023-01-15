d42d9c838a0b8daea8dca7fa7206085d3bdf9464
Dara-O <daraogunmola2@gmail.com>
2023-01-15 16:52:42 -0500

    converted reg file into an array

--- src/status_register_file.v
+++ src/status_register_file.v
@@ -13,33 +13,39 @@ module status_register_file #(parameter WORD_WIDTH=12, ADDR_WIDTH=3, TAG_WIDTH=1
 
     output  reg     [TAG_WIDTH-1:0]         o_tag,
     output  reg     [WORD_WIDTH-1:0]        o_data,
+    output  reg                             o_data_init, // 1 if the data is from an initialized word, else 0
     output  reg                             o_valid,
     output  wire                            o_freeze_inputs
 );
     
     assign o_freeze_inputs = i_halt;
     
-    reg [(2**ADDR_WIDTH)*WORD_WIDTH-1:0]    reg_file;
-    
+    reg [WORD_WIDTH-1:0] reg_file [0:(2**ADDR_WIDTH)-1];
+    reg [(2**ADDR_WIDTH)-1:0] r_reg_file_word_init; 
 
     always @(posedge clk, negedge arst_n) begin
         if(~arst_n) begin
-            reg_file <= {(2**ADDR_WIDTH)*WORD_WIDTH{1'h0}};
+            
+            r_reg_file_word_init <= {2**ADDR_WIDTH{1'h0}};
 
-            o_data  <= {WORD_WIDTH{1'h0}};
-            o_valid <= 1'h0;
+            o_data      <= {WORD_WIDTH{1'h0}};
+            o_data_init <= 1'h0;
+            o_valid     <= 1'h0;
         end
         else if(~i_halt & i_valid) begin
             if(i_wen) begin
-                reg_file[i_addr*WORD_WIDTH +: WORD_WIDTH]  <= i_data;
+                reg_file[i_addr] <= i_data;
+                r_reg_file_word_init[i_addr] <= i_valid;
 
                 // set output zero
-                o_data  <= {WORD_WIDTH{1'h0}};
-                o_valid <= 1'h0;
+                o_data      <= {WORD_WIDTH{1'h0}};
+                o_data_init <= 1'h0;
+                o_valid     <= 1'h0;
             end
             else begin
-                o_data  <= reg_file[i_addr*WORD_WIDTH +: WORD_WIDTH];
-                o_valid <= i_valid;
+                o_data      <= reg_file[i_addr];
+                o_data_init <= r_reg_file_word_init[i_addr];
+                o_valid     <= i_valid;
             end
         end
     end
