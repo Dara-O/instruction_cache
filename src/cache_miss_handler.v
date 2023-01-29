fb6935fabd64a8a699788d62cb9e1e3263a1aa41
Dara-O <daraogunmola2@gmail.com>
2023-01-29 01:51:44 -0500

    changes:
    - added lru_unit
    - added status
    - added miss_word_driver_m
    - connected modules
    
    Note:
    - yet to be tested

--- src/cache_miss_handler.v
+++ src/cache_miss_handler.v
@@ -10,7 +10,7 @@ module cache_miss_handler (
     input   wire    [SET_BITS_WIDTH-1:0]            i_set_bits,
     input   wire    [B_OFFSET_BITS_WIDTH-1:0]       i_block_offset_bits,
 
-    input   wire    [SA_WORD_WIDTH-1:0]             i_status_array_bits,
+    input   wire    [SA_WORD_WIDTH-1:0]             i_status_array_data,
     input   wire                                    i_valid,
 
     input   wire    [MEM_IF_DATA-1:0]               i_mem_if_data, // pre-decoded data
@@ -48,6 +48,8 @@ module cache_miss_handler (
     output  wire                                o_miss_state, // if 1, the cache is handling a miss, 
                                                               // if 0, cache can continue normal operation.
 
+    output  wire    [DA_SRAM_WORD_WIDTH-1:0]    o_missed_word,
+    output  wire                                o_missed_word_valid,
     output  wire                                o_ready
 );
     localparam SET_BITS_WIDTH = 4;
@@ -67,15 +69,17 @@ module cache_miss_handler (
     localparam MEM_IF_DATA = 32;
     localparam NUM_BLOCKS = 4;
 
+    localparam MEM_BLOCK_DATA_WIDTH = 320;
+
     
-    assign o_ready = ~i_halt; //FIXME
+    assign o_ready = ~i_halt | ~o_miss_state; //FIXME
 
     reg                             r_cache_hit;
     reg [TAG_BITS_WIDTH-1:0]        r_tag_bits;
     reg [SET_BITS_WIDTH-1:0]        r_set_bits;
     reg [B_OFFSET_BITS_WIDTH-1:0]   r_block_offset_bits;
     
-    reg [SA_WORD_WIDTH-1:0]         r_status_array_bits;
+    reg [SA_WORD_WIDTH-1:0]         r_status_array_data;
     reg                             r_valid;
 
     reg [MEM_IF_DATA-1:0]           r_mem_if_data;
@@ -88,7 +92,7 @@ module cache_miss_handler (
             r_set_bits              <= {SET_BITS_WIDTH{1'b0}};
             r_block_offset_bits     <= {B_OFFSET_BITS_WIDTH{1'b0}};
 
-            r_status_array_bits     <= {SA_WORD_WIDTH{1'b0}};
+            r_status_array_data     <= {SA_WORD_WIDTH{1'b0}};
             r_valid                 <= 1'b0;;
 
             r_mem_if_data           <= {MEM_IF_DATA{1'b0}};
@@ -100,7 +104,7 @@ module cache_miss_handler (
             r_set_bits              <= i_set_bits;
             r_block_offset_bits     <= i_block_offset_bits;
 
-            r_status_array_bits     <= i_status_array_bits;
+            r_status_array_data     <= i_status_array_data;
             r_valid                 <= i_valid;
 
             r_mem_if_data           <= i_mem_if_data;
@@ -108,80 +112,115 @@ module cache_miss_handler (
         end
     end
 
+    wire [NUM_BLOCKS-1:0] lru_block_replacement_mask;
+    wire lru_brm_valid;
+
+    lru_unit lru_unit_m(
+        .i_sa_data(r_status_array_data),
+        .i_sa_data_valid(r_valid),
+
+        .o_block_replacement_mask(lru_block_replacement_mask),
+        .o_brm_valid(lru_brm_valid)
+    );
+  
+
+    
+    wire mc_cu_mem_data_received;
+    wire mc_cu_mem_if_valid;
+    wire mc_cu_mem_ready;
+    
+    wire au_cu_arrays_update_complete;
+    wire au_cu_auc_valid;
+    
     wire cu_mc_initiate_mem_req;
     wire cu_mc_valid;
+    wire cu_miss_state;
+    wire cu_initiate_array_update; 
+    wire cu_send_missed_word;
+    wire cu_valid; 
+    wire cu_mem_if_ready; // connect me
+    wire cu_arrays_updater_ready; // connect me
+    wire cu_ready;
+
+    assign o_miss_state = cu_miss_state;
 
     control_unit ctrl_unit(
         .i_cache_hit(r_cache_hit),
         .i_valid(r_valid),
 
-        .i_mem_data_received(),
-        .i_mem_if_valid(),
+        .i_mem_data_received(mc_cu_mem_data_received),
+        .i_mem_if_valid(mc_cu_mem_if_valid),
 
-        .i_arrays_update_complete(), // au == arrays update complete
-        .i_auc_valid(),
+        .i_arrays_update_complete(au_cu_arrays_update_complete), // au == arrays update complete
+        .i_auc_valid(au_cu_auc_valid),
         
         .clk(clk),
         .arst_n(arst_n),
-        .i_halt(), //FIXME
+        .i_halt(), //FIXME all the modules should be able to halt it (only when they need to be accessed but arent ready)
         
-        .o_miss_state(),
+        .o_miss_state(cu_miss_state),
         
         .o_initiate_mem_req(cu_mc_initiate_mem_req),
         .o_mem_if_valid(cu_mc_valid),
         
-        .o_initiate_array_update(),
-        .o_send_missed_word(), // smw == send missed word
-        .o_valid(),
+        .o_initiate_array_update(cu_initiate_array_update),
+        .o_send_missed_word(cu_send_missed_word),
+        .o_valid(cu_valid),
 
-        .o_mem_if_ready(),
-        .o_arrays_udpater_ready(),
-        .o_ready()
+        .o_mem_if_ready(cu_mem_if_ready), //FIXME
+        .o_arrays_updater_ready(cu_arrays_updater_ready),
+        .o_ready(cu_ready)
     );
 
+    wire [MEM_BLOCK_DATA_WIDTH-1:0] mc_mem_block_data;
+    wire mc_mem_block_data_valid;
+
     memory_controller mem_ctrl(
-        .i_block_addr(),
-        .i_block_addr_valid(),
+        .i_block_addr({r_tag_bits. r_set_bits}),
+        .i_block_addr_valid(r_valid),
     
-        .i_initiate_req(),
-        .i_ir_valid(), // initiate request valid
-
-        .i_mem_data(), // burst mode
-        .i_mem_data_valid(),
-        .i_mem_data_received_ack(), // ??? in case there is a delay betweeen when the memory
-                                    // acknoledges our request and when the request is fulfilled
-
+        .i_initiate_req(cu_mc_initiate_mem_req),
+        .i_ir_valid(cu_mc_valid), // initiate request valid
+        
+        // from memory
+        .i_mem_data(i_mem_if_data),
+        .i_mem_data_valid(i_mem_if_valid),
+    
         .clk(clk),
         .arst_n(arst_n),
         .i_halt(), //FIXME
 
-        .o_mem_req_addr(), // burst mode
-        .o_mem_req_valid(),
-        .o_mem_ready(),
+        // to memory
+        .o_mem_req_addr(o_mem_if_addr), 
+        .o_mem_req_valid(o_mem_if_req_valid),
+        .o_mem_ready(o_mem_if_ready),
         
-        .o_mem_data_received(),
-        .o_mem_data_rcvd_valid(),
+        .o_mem_data_received(mc_cu_mem_data_received),
+        .o_mem_data_rcvd_valid(mc_cu_mem_if_valid),
+        .o_ir_ready(mc_cu_mem_ready),
 
-        .o_ready()
+        .o_mem_block_data(mc_mem_block_data), 
+        .o_mem_block_data_valid(mc_mem_block_data_valid)
     );
 
+    wire au_ready;
     arrays_updater arrays_updater_m(
-        .i_initiate_arrays_update(),
-        .i_iau_valid(),
+        .i_initiate_arrays_update(cu_initiate_array_update),
+        .i_iau_valid(cu_valid),
 
-        .i_set_addr(),
-        .i_set_addr_valid(),
+        .i_set_addr(r_set_bits),
+        .i_set_addr_valid(r_valid),
 
-        .i_tag_bits(),
-        .i_tag_bits_valid(),
+        .i_tag_bits(r_tag_bits),
+        .i_tag_bits_valid(r_valid),
 
-        .i_block_replacement_mask(),
-        .i_brm_valid(),
+        .i_block_replacement_mask(lru_block_replacement_mask),
+        .i_brm_valid(lru_brm_valid),
 
-        .i_mem_data(),
-        .i_mem_data_valid(),
+        .i_mem_data(mc_mem_block_data),
+        .i_mem_data_valid(mc_mem_block_data_valid),
 
-        .i_miss_state(),
+        .i_miss_state(cu_miss_state),
     
         .clk(clk),
         .arst_n(arst_n),
@@ -207,10 +246,19 @@ module cache_miss_handler (
         .o_da_valid(o_da_blocks_if_valid),
 
 
-        .o_arrays_update_complete(),
-        .o_auc_valid(), // arrays_update_complete_valid
+        .o_arrays_update_complete(au_cu_arrays_update_complete),
+        .o_auc_valid(au_cu_auc_valid), // arrays_update_complete_valid
 
-        .o_ready()
+        .o_ready(au_ready)
     );
 
+    miss_word_driver miss_word_driver_m(
+        .i_mem_data(mc_mem_block_data),
+        .i_block_offset_bits(r_block_offset_bits),
+        .i_valid(cu_send_missed_word & cu_valid & r_valid & mc_mem_block_data_valid),
+
+        .o_missed_word(o_missed_word),
+        .o_valid(o_missed_word_valid)
+    );
+  
 endmodule
