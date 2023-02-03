159bcc0dfd2cd1d2111a87b59ddffc8d48281d29
Dara-O <daraogunmola2@gmail.com>
2023-02-02 23:22:35 -0500

    renamed signals from ta to sa

--- src/use_bit_updater.v
+++ src/use_bit_updater.v
@@ -6,8 +6,8 @@ module use_bit_updater(
     input   wire                            i_cache_hit,
     input   wire                            i_valid,
     
-    output  wire    [SA_WORD_WIDTH-1:0]     o_ta_w_data,
-    output  wire    [NUM_WAYS-1:0]          o_ta_w_mask,
+    output  wire    [SA_WORD_WIDTH-1:0]     o_sa_w_data,
+    output  wire    [NUM_WAYS-1:0]          o_sa_w_mask,
     output  wire                            o_valid
 );
 
@@ -19,25 +19,25 @@ module use_bit_updater(
     localparam VALID_BIT_IDX    = 1;
 
     assign o_valid = i_valid & i_cache_hit;
-    assign o_ta_w_mask = {NUM_WAYS{1'b1}};
+    assign o_sa_w_mask = {NUM_WAYS{1'b1}};
 
-    wire [NUM_WAYS-1:0] w_ta_use_bits = {
+    wire [NUM_WAYS-1:0] w_sa_use_bits = {
         &i_sa_data[7:6], // VALID_BIT & USE_BIT. 
         &i_sa_data[5:4],
         &i_sa_data[3:2],
         &i_sa_data[1:0]
     };
 
-    wire [$clog2(NUM_WAYS):0] w_num_ones =  w_ta_use_bits[0] + 
-                                            w_ta_use_bits[1] + 
-                                            w_ta_use_bits[2] + 
-                                            w_ta_use_bits[3];
+    wire [$clog2(NUM_WAYS):0] w_num_ones =  w_sa_use_bits[0] + 
+                                            w_sa_use_bits[1] + 
+                                            w_sa_use_bits[2] + 
+                                            w_sa_use_bits[3];
 
     wire [NUM_WAYS-1:0] w_new_use_bits; 
-    assign w_new_use_bits= (w_num_ones < NUM_WAYS-1) ? (w_ta_use_bits | i_hit_blocks) : 
+    assign w_new_use_bits= (w_num_ones < NUM_WAYS-1) ? (w_sa_use_bits | i_hit_blocks) : 
                                                        (i_hit_blocks);
 
-    assign o_ta_w_data = {
+    assign o_sa_w_data = {
         i_sa_data[7] ,w_new_use_bits[3],
         i_sa_data[5] ,w_new_use_bits[2],
         i_sa_data[3] ,w_new_use_bits[1],
